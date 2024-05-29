0c21b299bb02fb0138e886bb79b79b828fd6a10d
Mikey Stengel <mikey@serlo.org>
2024-05-29 18:43:57 +0200

    chore: Upgrade editor web component & show example on how to undo/redo

--- index.js
+++ index.js
@@ -29,11 +29,38 @@ class TestElement extends LitElement {
     this.editing = false;
   }
 
+  getCurrentEditorState() {
+    const editor = this.shadowRoot.querySelector("serlo-editor");
+    if (editor) {
+      console.log("Current state:", editor.currentState);
+    }
+  }
+
+  undo() {
+    const editor = this.shadowRoot.querySelector("serlo-editor");
+    if (editor && editor.history) {
+      editor.history.dispatchUndo();
+    }
+  }
+
+  redo() {
+    const editor = this.shadowRoot.querySelector("serlo-editor");
+    if (editor && editor.history) {
+      editor.history.dispatchRedo();
+    }
+  }
+
   render() {
     return html`
       <button @click=${() => (this.editing = !this.editing)}>
         ${this.editing ? "READ" : "EDIT"}
       </button>
+      <button @click=${this.getCurrentEditorState.bind(this)}>
+        Get Current State
+      </button>
+      <button @click=${this.undo.bind(this)}>Undo</button>
+      <button @click=${this.redo.bind(this)}>Redo</button>
+
       <serlo-editor
         mode=${this.editing ? "write" : "read"}
         initial-state=${JSON.stringify(initialExampleState)}
